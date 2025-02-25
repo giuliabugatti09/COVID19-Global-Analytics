7b2434364f2898b4d230d4d34eb245b1220111de
Giulia Bugatti Fonseca <giuliabugatti02@gmail.com>
2025-02-25 19:49:54 -0300

    Update covid.py

--- scripts/covid.py
+++ scripts/covid.py
@@ -2,11 +2,7 @@
   "nbformat": 4,
   "nbformat_minor": 0,
   "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMcRvwxwJ32iBz6tnABTerG",
-      "include_colab_link": true
-    },
+  
     "kernelspec": {
       "name": "python3",
       "display_name": "Python 3"
